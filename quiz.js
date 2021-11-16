abf7d0634f8b297be28f669b2a0cdb9c197c35c5
Jkgdev8 <juliennegarcia@Juliennes-MBP.socal.rr.com>
2021-11-15 23:28:27 -0800

    started questions

---
+++ quiz.js
@@ -0,0 +1,16 @@
+var question = document.querySelector("#question");
+var choices = Array.from(document.querySelectorAll('.choice-text'));
+var progressText = document.querySelector('#progressText');
+var scoreText = document.querySelector('#score');
+
+
+var currentQuestion = {};
+var acceptingAnswers = true;
+var score = 0;
+var questionCounter = 0;
+var availbleQuestion = [];
+var questions = [
+    {
+        question:
+    }
+]
